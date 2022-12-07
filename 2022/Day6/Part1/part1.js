20d2ff1991f66b2c793bc4ad0e8b4e979574bdc5
Jordan <j.gregorywallis@gmail.com>
2022-12-07 14:22:12 +0000

    Day 6 part 1 complete and correct answer found

---
+++ 2022/Day6/Part1/part1.js
@@ -0,0 +1,23 @@
+const {readFileSync} = require('fs');
+
+const puzzleInput = readFileSync('puzzle-input.txt', 'utf8');
+
+const characters = puzzleInput.split("");
+
+var postion = 0;
+
+for(let i = 0; i < characters.length; i++)
+{
+    var group = characters.slice(i, i + 4);
+    let findDuplicates = arr => arr.filter((item, index) => arr.indexOf(item) != index)
+
+    var duplicates = findDuplicates(group);
+
+    if(duplicates.length == 0){
+        postion = i;
+        console.log(group);
+        break;
+    }
+}
+
+console.log(postion + 4);
