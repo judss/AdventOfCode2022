ac78b853588c9ed189b4477e661eda00f4e3b328
Jordan <j.gregorywallis@gmail.com>
2022-12-11 18:04:06 +0000

    Finished but not found the correct answer

--- Day9/Part1/part1.cpp
+++ Day9/Part1/part1.cpp
@@ -2,7 +2,35 @@
 #include <fstream>
 #include "string"
 #include <list>
+#include <algorithm>
 using namespace std;
+
+class Position {  
+    public:
+        int x;
+        int y;
+};
+
+bool checkIfHeadIsOverLappingTail(Position newHeadPosition, Position currentTailPosition){
+    return currentTailPosition.x == newHeadPosition.x && currentTailPosition.y == newHeadPosition.y;
+}
+
+bool checkIfInSameColumn(Position newHeadPosition, Position currentTailPosition){
+    return newHeadPosition.y == currentTailPosition.y;
+}
+
+bool checkIfInSameRow(Position newHeadPosition, Position currentTailPosition){
+    return newHeadPosition.x == currentTailPosition.x;
+}
+
+bool checkIfAboveOrBelow(Position newHeadPosition, Position currentTailPosition){
+    return newHeadPosition.y == currentTailPosition.y - 1 || newHeadPosition.y == currentTailPosition.y + 1;
+}
+
+bool checkIfLeftOrRight(Position newHeadPosition, Position currentTailPosition){
+    return newHeadPosition.x == currentTailPosition.x - 1 || newHeadPosition.x == currentTailPosition.x + 1;
+}
+
 
 int main() 
 {
@@ -13,9 +41,13 @@ int main()
     const string above = "A";
     const string underneath = "U";
 
-    list<position*> visitedTailPositions;
-    position *currentHeadPosition = new position(0,0);
-    position *currentTailPosition = new position(0,0);
+    list<Position> visitedTailPositions;
+    Position currentHeadPosition;
+    currentHeadPosition.x = 0;
+    currentHeadPosition.y = 0;
+    Position currentTailPosition;
+    currentTailPosition.x = 0;
+    currentTailPosition.y = 0;
 
     visitedTailPositions.emplace_back(currentTailPosition);
 
@@ -23,43 +55,115 @@ int main()
 
     for (string line; std::getline(PuzzleInput, line); )
     {
-        position *newHeadPosition = new position();
-        position *newTailPosition = new position();
-
-        const char *moves = line.c_str();
         // get direction to go in
-        char directionOfMove = moves[0];
+        char directionOfMove = line[0];
 
         // convert char to int using ascii code
-        int numberOfMoves = moves[1] - '0';
+        int numberOfMoves = line[2] - '0';
 
-        switch(directionOfMove)
+        for(int i = 0; i < numberOfMoves; i++)
         {
-            case 'L':
-            break;
-            case 'R':
-            break;
-            case 'U':
-            break;
-            case 'D':
-            break;
+            Position newHeadPosition = currentHeadPosition;
+            Position newTailPosition = currentTailPosition;
+
+            bool isOverLapping = false;
+            bool onSameColumn = false;
+            bool onSameRow = false;
+            bool isAboveOrBelow = false;
+            bool isToLeftOrRight = false;
+
+            switch(directionOfMove)
+            {
+                case 'L':
+                    newHeadPosition.x -= 1;
+
+                    isOverLapping = checkIfHeadIsOverLappingTail(newHeadPosition, currentTailPosition);
+                    onSameColumn = checkIfInSameColumn(newHeadPosition, currentTailPosition);
+                    isAboveOrBelow = checkIfAboveOrBelow(newHeadPosition, currentTailPosition);
+
+                    if(!isOverLapping){
+                        if((newHeadPosition.x == currentTailPosition.x - 2 && isAboveOrBelow) || newHeadPosition.y == currentTailPosition.y){
+                            newTailPosition = newHeadPosition;
+                            newTailPosition.x += 1;
+                        }
+                    }
+                    break;
+                case 'R':
+                    newHeadPosition.x += 1;
+
+                    isOverLapping = checkIfHeadIsOverLappingTail(newHeadPosition, currentTailPosition);
+                    onSameColumn = checkIfInSameColumn(newHeadPosition, currentTailPosition);
+                    isAboveOrBelow = checkIfAboveOrBelow(newHeadPosition, currentTailPosition);
+
+                    if(!isOverLapping){
+                        if((newHeadPosition.x == currentTailPosition.x + 2 && isAboveOrBelow) || onSameColumn){
+                            newTailPosition = newHeadPosition;
+                            newTailPosition.x -= 1;
+                        }
+                    }
+                    break;
+                case 'U':
+                    newHeadPosition.y += 1;
+
+                    isOverLapping = checkIfHeadIsOverLappingTail(newHeadPosition, currentTailPosition);
+                    onSameRow = checkIfInSameRow(newHeadPosition, currentTailPosition);
+                    isToLeftOrRight = checkIfLeftOrRight(newHeadPosition, currentTailPosition);
+
+                    if(!isOverLapping){
+                        if((newHeadPosition.y == currentTailPosition.y + 2 && isToLeftOrRight) || onSameRow){
+                            newTailPosition = newHeadPosition;
+                            newTailPosition.y -= 1;
+                        }                        
+                    }                  
+                    break;
+                case 'D':
+                    newHeadPosition.y -= 1;
+
+                    isOverLapping = checkIfHeadIsOverLappingTail(newHeadPosition, currentTailPosition);
+                    onSameRow = checkIfInSameRow(newHeadPosition, currentTailPosition);
+                    isToLeftOrRight = checkIfLeftOrRight(newHeadPosition, currentTailPosition);
+
+                    if(!isOverLapping){
+                        if((newHeadPosition.y == currentTailPosition.y - 2 && isToLeftOrRight) || onSameRow){
+                            newTailPosition = newHeadPosition;
+                            newTailPosition.y += 1;
+                        }                        
+                    }  
+                    break;
+            }
+
+            currentHeadPosition = newHeadPosition;
+            currentTailPosition = newTailPosition;
+            visitedTailPositions.emplace_back(newTailPosition);
+        }
+    }
+
+    // Sort the elements in the list by their x and y properties.
+    visitedTailPositions.sort([](const Position& a, const Position& b) 
+    {
+        if (a.x < b.x) {
+            return true;
         }
 
-        //result += ">> " + line + "\n";
-    }
+        if (a.x == b.x && a.y < b.y) {
+            return true;
+        }
+        
+        return false;
+    });
+
+    // erase any duplicates
+    visitedTailPositions.erase(std::unique(visitedTailPositions.begin(), visitedTailPositions.end(), 
+                            [](const Position& a, const Position& b) {
+                                return a.x == b.x && a.y == b.y;
+                            }),
+                            visitedTailPositions.end());
+
+     
+
+    int answer = visitedTailPositions.size();
+
+    std::cout << "Answer: " << answer;
 
     PuzzleInput.close();
 }
-
-class position {  
-    int x;
-    int y;
-  public:
-    position(int, int);
-    position();
-};
-
-position::position(int x, int y){
-    x = x;
-    y = y;
-};
